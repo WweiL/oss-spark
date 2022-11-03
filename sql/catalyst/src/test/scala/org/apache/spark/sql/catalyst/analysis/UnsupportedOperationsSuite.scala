b52553f4750553a33de846502e337291933d2f5a
Wei Liu <wei.liu@databricks.com>
2022-11-03 11:26:36 -0700

    fix

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/UnsupportedOperationsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/UnsupportedOperationsSuite.scala
@@ -652,13 +652,6 @@ class UnsupportedOperationsSuite extends SparkFunSuite with SQLHelper {
     assertPassOnGlobalWatermarkLimit(
       "deduplicate after streaming aggregation in Append mode",
       Deduplicate(Seq(attribute), streamRelation.groupBy("a")(count("*"))))
-
-//    assertFailOnGlobalWatermarkLimit(
-//      "FlatMapGroupsWithState after streaming aggregation in Append mode",
-//      TestFlatMapGroupsWithState(
-//        null, att, att, Seq(att), Seq(att), att, null, Append,
-//        isMapGroupsWithState = false, null,
-//        streamRelation.groupBy("a")(count("*"))))
   }
 
   // FlatMapGroupsWithState
